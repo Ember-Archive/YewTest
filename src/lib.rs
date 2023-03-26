fe3296812f373b6b3cb1d667e7420485284028f0
Ember Hext <emberhext@gmail.com>
2023-03-25 23:24:40 -0400

    some more experimenting

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
+use gloo::console::log;
 use yew::prelude::*;
+
+
 
 #[function_component(App)]
 pub fn app() -> Html {
+    
+    let tasks: Vec<&str> = vec!["clean room", "grocery shopping", "write Rust"];
+
     html! {
-        <h1>{ "Hello World!" }</h1>
+        <>
+            <h1>{ "Hello World!" }</h1>
+            <ul>
+                {list_to_html(tasks)}
+            </ul>
+        </>
     }
+}
+
+fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+    list.iter().map(|task| html!{<li>{task}</li>}).collect()
 }
